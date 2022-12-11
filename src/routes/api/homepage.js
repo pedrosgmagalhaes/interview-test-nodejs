498af8472b4550291ea14e3e74957732759e8505
Pedro Magalhaes <pedrosgmagalhaes@gmail.com>
2022-12-11 11:44:25 -0300

    added criterias for post and users

--- src/routes/api/homepage.js
+++ src/routes/api/homepage.js
@@ -11,11 +11,27 @@ router.get('/test', (req, res) => {
     }
 });
 
-// // GET request for homepage feed
 router.get('/getFeeds', async (req, res) => {
+    // Retrieve query parameters from the request
+    const { onlyMine, startDate, endDate } = req.query;
 
-    // Query to return 10 posts
-    const query = Post.find().limit(10);
+    // Create the base query that returns 10 posts by default
+    let query = Post.find().limit(10);
+
+    // If onlyMine is truthy and req.user is defined, add a condition to the query to only return posts written by the current user
+    if (onlyMine && req.user) {
+        query = query.where('user', req.user._id);
+    }
+
+    // If startDate is provided, add a condition to the query to only return posts posted after the start date
+    if (startDate) {
+        query = query.where('date').gte(startDate);
+    }
+
+    // If endDate is provided, add a condition to the query to only return posts posted before the end date
+    if (endDate) {
+        query = query.where('date').lte(endDate);
+    }
 
     // Execute the query and handle the result
     query.exec((err, posts) => {
@@ -28,6 +44,35 @@ router.get('/getFeeds', async (req, res) => {
             res.status(200).send(posts);
         }
     });
+});
+
+// POST request for creating a post
+router.post('/createPost', async (req, res) => {
+    console.log(req.body);
+    // Check if the user has reached the daily post limit
+    const today = new Date();
+    const startOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const userPostCount = await Post.countDocuments({ user: req.body.user, date: { $gte: startOfToday } });
+
+    if (userPostCount >= 5) {
+        return res.status(400).send({ error: 'You have reached the daily post limit.' });
+    }
+
+    // Create a new post
+    const post = new Post({
+        content: req.body.content,
+        user: req.body.user,
+        type: req.body.type
+    });
+
+    // Save the post to the database
+    try {
+        const savedPost = await post.save();
+        return res.status(200).send(savedPost);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Error occurred while creating the post.');
+    }
 });
 
 module.exports = router;
